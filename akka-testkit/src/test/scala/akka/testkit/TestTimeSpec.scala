1c8e1081d2b5eb02d4455cda16ecaa99ec0a1802
Patrik Nordwall <patrik.nordwall@gmail.com>
2013-12-11 15:58:54 +0100

    =tes #3727 Fix timing failure in TestTimeSpec

--- akka-testkit/src/test/scala/akka/testkit/TestTimeSpec.scala
+++ akka-testkit/src/test/scala/akka/testkit/TestTimeSpec.scala
@@ -11,14 +11,8 @@ class TestTimeSpec extends AkkaSpec(Map("akka.test.timefactor" -> 2.0)) with Bef
 
   "A TestKit" must {
 
-    "correctly dilate times" taggedAs TimingTest in {
-      val probe = TestProbe()
-      val now = System.nanoTime
-      intercept[AssertionError] { probe.awaitCond(false, Duration("1 second")) }
-      val diff = System.nanoTime - now
-      val target = (1000000000l * testKitSettings.TestTimeFactor).toLong
-      diff must be > (target - 300000000l)
-      diff must be < (target + 300000000l)
+    "correctly dilate times" in {
+      1.second.dilated.toNanos must be(1000000000L * testKitSettings.TestTimeFactor)
     }
 
     "awaitAssert must throw correctly" in {
